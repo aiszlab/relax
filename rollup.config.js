2f877a3aef7a51395769d9f70785af50af246307
murukal <tutu@fantufantu.com>
2023-07-30 20:36:37 +0800

    feat(build): use rollup

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import pkg from './package.json' assert { type: 'json' }
+
+/** @type {import("rollup").RollupOptions} */
+const configuration = {
+  input: './src/index.ts',
+
+  output: {
+    format: 'es',
+    dir: './dist',
+    preserveModules: true,
+    preserveModulesRoot: './src'
+  },
+
+  plugins: [
+    resolve({
+      extensions: ['.ts']
+    }),
+    typescript(),
+    babel({
+      babelHelpers: 'bundled',
+      presets: ['@babel/preset-env', '@babel/preset-typescript']
+    })
+  ],
+
+  external: [...Object.keys(pkg.dependencies), ...Object.keys(pkg.peerDependencies)]
+}
+
+export default configuration
