1c82b5bcd285137881968ea0600b82c5feb014d7
murukal <tutu@fantufantu.com>
2023-10-21 21:20:42 +0800

    feat: improve controlled state

--- packages/relax/src/hooks/use-controlled-state.ts
+++ packages/relax/src/hooks/use-controlled-state.ts
@@ -12,26 +12,18 @@ interface Props<T> {
  * @description
  * controlled state
  */
-export const useControlledState = <T>(
-  controlledState: State<T>,
-  { defaultState }: Props<T> = {}
-): [T, Dispatch<SetStateAction<T>>] => {
+export const useControlledState = <T>(controlledState: State<T>): [T, Dispatch<SetStateAction<T>>] => {
   /// initialize state
   const [state, setState] = useState<T>(() => {
     if (isStateGetter(controlledState)) return controlledState()
-
-    if (isUndefined(controlledState)) {
-      if (isUndefined(defaultState)) return controlledState
-      if (isStateGetter(defaultState)) return defaultState()
-      return defaultState
-    }
-
     return controlledState
   })
 
   useEffect(() => {
     // when state is not controlled
     if (isUndefined(controlledState)) return
+    // if input state is function, mean it is not controlled
+    if (isStateGetter(controlledState)) return
     // if state is equal with value
     if (controlledState === state) return
     /// update inner state
