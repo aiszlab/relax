5d37f10029c317de1f4843227fe8ccf6e19c1211
murukal <tutu@fantufantu.com>
2023-12-05 23:42:31 +0800

    feat: add direction for scrollable hook

--- packages/relax/src/hooks/use-scrollable.ts
+++ packages/relax/src/hooks/use-scrollable.ts
@@ -1,46 +1,84 @@
 import { Key, useCallback, useRef } from 'react'
 
+type Direction = 'horizontal' | 'vertical'
+
+interface Props {
+  direction?: Direction
+}
+
+const PROPERTY = new Map<
+  Direction,
+  {
+    target: Extract<keyof HTMLElement, 'offsetTop' | 'offsetLeft'>
+    trigger: Extract<keyof HTMLElement, 'scrollTop' | 'scrollLeft'>
+  }
+>([
+  [
+    'vertical',
+    {
+      trigger: 'scrollTop',
+      target: 'offsetTop'
+    }
+  ],
+  [
+    'horizontal',
+    {
+      trigger: 'scrollLeft',
+      target: 'offsetLeft'
+    }
+  ]
+])
+
 /**
  * @description
  * scrollable hook
  */
-export const useScrollable = <P extends HTMLElement, C extends HTMLElement>() => {
+export const useScrollable = <P extends HTMLElement, C extends HTMLElement>({ direction = 'vertical' }: Props = {}) => {
   const groupRef = useRef<P>(null)
   const itemRefs = useRef<Map<Key, C | null>>(new Map())
   const scroller = useRef<number | null>(null)
 
-  const scrollTo = useCallback((to: number, duration = 0) => {
-    if (scroller.current) {
-      cancelAnimationFrame(scroller.current)
-    }
+  const scrollTo = useCallback(
+    (to: number, duration = 0) => {
+      if (scroller.current) {
+        cancelAnimationFrame(scroller.current)
+      }
 
-    const group = groupRef.current
-    if (!group) return
+      const group = groupRef.current
+      if (!group) return
+      const triggerProperty = PROPERTY.get(direction)!.trigger
 
-    // if duration <= 0, jump immediately
-    if (duration <= 0) {
+      // if duration <= 0, jump immediately
+      if (duration <= 0) {
+        scroller.current = requestAnimationFrame(() => {
+          group[triggerProperty] = to
+        })
+        return
+      }
+
+      // animate
+      const currentAt = group[triggerProperty]
+      const difference = to - currentAt
+      const step = (difference / duration) * 10
+
       scroller.current = requestAnimationFrame(() => {
-        group.scrollTop = to
+        group[triggerProperty] = currentAt + step
+        if (group[triggerProperty] === to) return
+        scrollTo(to, duration - 10)
       })
-      return
-    }
+    },
+    [direction]
+  )
 
-    // animate
-    const difference = to - group.scrollTop
-    const step = (difference / duration) * 10
-
-    scroller.current = requestAnimationFrame(() => {
-      group.scrollTop = group.scrollTop + step
-      if (group.scrollTop === to) return
-      scrollTo(to, duration - 10)
-    })
-  }, [])
-
-  const to = useCallback((key: Key) => {
-    const item = itemRefs.current.get(key)
-    if (!item) return 0
-    return item.offsetTop
-  }, [])
+  const to = useCallback(
+    (key: Key) => {
+      const item = itemRefs.current.get(key)
+      if (!item) return 0
+      // different direction, use different property
+      return item[PROPERTY.get(direction)!.target]
+    },
+    [direction]
+  )
 
   return {
     groupRef,
