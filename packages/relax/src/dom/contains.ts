702a6009c8d20dc0973efebf15b3d54d5ec3e066
murukal <tutu@fantufantu.com>
2024-02-15 09:27:38 +0800

    feat: add dom api contains

---
+++ packages/relax/src/dom/contains.ts
@@ -0,0 +1,21 @@
+export const contains = (root: Node | null | undefined, n?: Node) => {
+  if (!root) {
+    return false
+  }
+
+  // Use native if support
+  if (root.contains) {
+    return root.contains(n ?? null)
+  }
+
+  // `document.contains` not support with IE11
+  let node = n
+  while (node) {
+    if (node === root) {
+      return true
+    }
+    node = node.parentNode as Node
+  }
+
+  return false
+}
