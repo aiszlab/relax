a6bd3e70884dde20fd04958b8982cbb6148bdf54
murukal <tutu@fantufantu.com>
2024-03-03 14:30:53 +0800

    feat: change force update api

--- packages/relax/src/hooks/use-force-update.ts
+++ packages/relax/src/hooks/use-force-update.ts
@@ -1,18 +1,17 @@
 import { useCallback, useState } from 'react'
 
+type UsedForceUpdate = [number, () => void]
+
 /**
  * @description
  * force update
  */
-export const useForceUpdate = () => {
+export const useForceUpdate = (): UsedForceUpdate => {
   const [times, setTimes] = useState(1)
 
   const forceUpdate = useCallback(() => {
     setTimes((prev) => prev + 1)
   }, [])
 
-  return {
-    times,
-    forceUpdate
-  }
+  return [times, forceUpdate]
 }
