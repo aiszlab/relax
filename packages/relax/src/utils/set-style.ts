782f27cbe0edbedf0103621d8ae1fefe08327c4f
murukal <tutu@fantufantu.com>
2024-04-23 14:44:02 +0800

    feat: add scrollbar size counter

---
+++ packages/relax/src/utils/set-style.ts
@@ -0,0 +1,17 @@
+import { Voidable } from '../types'
+
+/**
+ * @description
+ * set inline style util
+ */
+export const setStyle = (target: HTMLElement, styles: Voidable<Partial<CSSStyleDeclaration>>) => {
+  if (!styles) return {}
+
+  return Object.entries(styles).reduce<Partial<CSSStyleDeclaration>>((prev, [key, value]) => {
+    // @ts-ignore
+    prev[key] = target.style[key]
+    // @ts-ignore
+    target.style[key] = value
+    return prev
+  }, {})
+}
