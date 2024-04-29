e22677c9eae4dc6534deba3c07f0bcc1a4b8d3c0
murukal <tutu@fantufantu.com>
2024-04-29 15:06:03 +0800

    feat: add clone api

---
+++ packages/relax/src/is/is-primitive.ts
@@ -0,0 +1,7 @@
+/**
+ * @description
+ * string/number/... is primitive type
+ */
+export const isPrimitive = (value: unknown) => {
+  return value !== Object(value)
+}
---
+++ packages/relax/src/utils/clone.ts
@@ -0,0 +1,19 @@
+import { isArray } from '../is/is-array'
+import { isPrimitive } from '../is/is-primitive'
+
+/**
+ * @description
+ * in develop, there are many cases to clone a string/number/object/array
+ * this util is planning to resolve it
+ */
+export const clone = (value: unknown) => {
+  if (isPrimitive(value)) return value
+
+  if (value instanceof Map) return new Map(value)
+
+  if (value instanceof Set) return new Set(value)
+
+  if (isArray(value)) return Array.from(value)
+
+  return Object(value)
+}
