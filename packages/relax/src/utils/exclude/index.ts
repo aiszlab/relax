1ccf0b325d492d449ede54f3f643240b906c0eb6
murukal <tutu@fantufantu.com>
2024-05-23 00:09:17 +0800

    feat: add exclude declaration & docs

--- packages/relax/src/utils/exclude/index.ts
+++ packages/relax/src/utils/exclude/index.ts
@@ -3,8 +3,11 @@
  * exclude any value from array
  *
  * for performance, avoid using `Array.includes`, replace with `Set`
+ *
+ * @example
+ * exclude([1, 2, 3, 4, 5], [2, 4]) // [1, 3, 5]
  */
-export const exclude = (value: Array<unknown>, _excludeBy: Array<unknown>) => {
+export const exclude = <T>(value: Array<T>, _excludeBy: Array<unknown>) => {
   const excludeBy = new Set(_excludeBy)
   return value.filter((item) => !excludeBy.has(item))
 }
