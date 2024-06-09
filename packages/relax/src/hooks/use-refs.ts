ec5fa3772ad7c1327ddfe3e976a3fb8866ba22b2
murukal <tutu@fantufantu.com>
2024-06-09 19:27:56 +0800

    fix: refs props type add string check

--- packages/relax/src/hooks/use-refs.ts
+++ packages/relax/src/hooks/use-refs.ts
@@ -1,18 +1,26 @@
 import { type MutableRefObject, type RefCallback, useMemo } from 'react'
 import { isFunction } from '../is/is-function'
-import type { Nullable } from '../types'
+import type { Nullable, Voidable } from '../types'
 
-type Refable<T> = RefCallback<T> | MutableRefObject<T>
+type Refable<T> = RefCallback<T> | MutableRefObject<T> | string
 
 const mount = <T>(ref: Refable<T>, trigger: T) => {
   if (isFunction(ref)) {
     ref(trigger)
     return
   }
+
+  // in deprecated react, class component can use string ref
+  // but there are many problems, in relax, we just make it not work
+  // issue for react: https://github.com/facebook/react/pull/8333#issuecomment-271648615
+  if (typeof ref === 'string') {
+    return
+  }
+
   ref.current = trigger
 }
 
-export const useRefs = <T>(...refs: Nullable<Refable<Nullable<T>>>[]) => {
+export const useRefs = <T>(...refs: Voidable<Refable<Nullable<T>>>[]) => {
   return useMemo(() => {
     return (trigger: T) => {
       refs.forEach((ref) => {
