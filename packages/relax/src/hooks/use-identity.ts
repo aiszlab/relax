8e2e456dc8fceefffbbd73f91fed7c2793e10fd6
murukal <tutu@fantufantu.com>
2024-06-22 09:30:01 +0800

    feat: id signal cound be empty

--- packages/relax/src/hooks/use-identity.ts
+++ packages/relax/src/hooks/use-identity.ts
@@ -9,11 +9,14 @@ type Identity = {
  * @description
  * extends from react `useId`
  */
-export const useIdentity = (signal: string): [string, () => string] => {
+export const useIdentity = (signal = ''): [string, () => string] => {
   const id = useId()
   const count = useRef(0)
 
   const unique = useCallback(() => {
+    if (!signal) {
+      return `${id}${count.current++}`
+    }
     return `${id}${signal}:${count.current++}`
   }, [])
 
