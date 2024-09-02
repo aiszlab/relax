81109bd27327844196900a98312d923ae6593099
murukal <tutu@fantufantu.com>
2024-09-02 22:38:25 +0800

    chore: release 1.3.0

--- packages/wasp/src/modules/development.ts
+++ packages/wasp/src/modules/development.ts
@@ -1,15 +1,17 @@
 import { createServer, defineConfig, mergeConfig } from "vite";
-import React from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
 
 export const development = async () => {
   const server = await createServer(
     mergeConfig(
       {},
       defineConfig({
-        plugins: [React()],
+        plugins: [react()],
       }),
     ),
   );
 
-  await server.listen();
+  await server.listen(8800);
+
+  console.log("启动成功");
 };
