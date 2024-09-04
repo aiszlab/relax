d8c1bc204e7b3bc86a1f492366ce49bcec535829
murukal <tutu@fantufantu.com>
2024-09-04 21:54:42 +0800

    feat: add autoprefixer

--- packages/wasp/src/configurations/vite.config.ts
+++ packages/wasp/src/configurations/vite.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
-import tailwindConfiguration from "./tailwind.config.js";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
-      plugins: [
-        tailwindcss({
-          config: tailwindConfiguration,
-        }),
-      ],
+      plugins: [tailwindcss, autoprefixer],
     },
   },
 });
