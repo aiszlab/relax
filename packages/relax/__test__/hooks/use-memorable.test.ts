2d5adffe159b0f36298aea917f9f4fd3fe3c925a
murukal <tutu@fantufantu.com>
2024-09-13 20:34:52 +0800

    feat: add unit test

--- packages/relax/__test__/hooks/use-memorable.test.ts
+++ packages/relax/__test__/hooks/use-memorable.test.ts
@@ -3,17 +3,47 @@
  */
 
 import { renderHook } from "@testing-library/react";
-import { useMemorable } from "../../src";
+import { useCounter, useMemorable } from "../../src";
 import { describe, it, expect, jest } from "@jest/globals";
+import { act } from "react";
 
 describe("useMemorable", () => {
-  it("performance", async () => {
+  it("performance", () => {
     const getter = jest.fn();
     const reconciler = jest.fn(() => true);
 
-    renderHook(() => useMemorable(() => getter(), [], reconciler));
+    const { result } = renderHook(() => {
+      const counter = useCounter(0);
+      useMemorable(() => getter(), [counter[0]], reconciler);
+      return counter;
+    });
 
     expect(getter).toHaveBeenCalledTimes(1);
     expect(reconciler).toHaveBeenCalledTimes(0);
+
+    act(() => {
+      result.current[1].add();
+    });
+
+    expect(getter).toHaveBeenCalledTimes(2);
+    expect(reconciler).toHaveBeenCalledTimes(1);
+  });
+
+  it("value update", () => {
+    const reconciler = jest.fn(() => true);
+
+    const { result } = renderHook(() => {
+      const counter = useCounter(0);
+      const _value = useMemorable(
+        () => {
+          return counter[0];
+        },
+        [counter[0]],
+        () => {
+          return true;
+        },
+      );
+      return [];
+    });
   });
 });
