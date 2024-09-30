e63cfa7eebdde291060d26c2d9d587a7b1b59aa5
murukal <tutu@fantufantu.com>
2024-09-30 21:57:43 +0800

    chore: path unit test

--- packages/relax/.babelrc.cjs
+++ packages/relax/.babelrc.cjs
@@ -6,8 +6,10 @@ const pkg = require("./package.json");
 const config = (api) => {
   api.cache.never();
 
+  // README: current package why add @babel/preset-react
+  // in unit tests, use jsx, need convert to js
   return {
-    presets: ["@babel/preset-env", "@babel/preset-typescript"],
+    presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
     plugins: [["@babel/plugin-transform-runtime", { version: pkg.dependencies["@babel/runtime"] }]],
   };
 };
