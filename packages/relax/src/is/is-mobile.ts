aecb61921bd3d4b68ba7ffe9d3b53fc3e287ef8d
murukal <tutu@fantufantu.com>
2024-10-08 20:27:06 +0800

    chore: add is-mobile empty ua result

--- packages/relax/src/is/is-mobile.ts
+++ packages/relax/src/is/is-mobile.ts
@@ -7,14 +7,9 @@ export const isMobile = () => {
     return false;
   }
 
-  const agent =
-    navigator.userAgent ||
-    navigator.vendor ||
-    (
-      window as unknown as {
-        opera: string;
-      }
-    ).opera;
+  const agent = navigator.userAgent || navigator.vendor;
+
+  if (!agent) return false;
 
   return (
     /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
