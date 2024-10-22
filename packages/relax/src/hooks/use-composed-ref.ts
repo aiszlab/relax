524be53e12e959fc455f5c6246eaa4cf206b7080
murukal <tutu@fantufantu.com>
2024-10-22 16:26:06 +0800

    refactor: change composed ref name

--- packages/relax/src/hooks/use-composed-ref.ts
+++ packages/relax/src/hooks/use-composed-ref.ts
@@ -2,7 +2,7 @@ import type { Nullable, Voidable } from "@aiszlab/relax/types";
 import { mountRef, type Refable } from "../react/mount-ref";
 import { useEvent } from "./use-event";
 
-export const useComposeRef = <T>(...refs: Voidable<Refable<Nullable<T>>>[]) => {
+export const useComposedRef = <T>(...refs: Voidable<Refable<Nullable<T>>>[]) => {
   return useEvent((reference: T) => {
     refs.forEach((ref) => {
       if (!ref) return;
