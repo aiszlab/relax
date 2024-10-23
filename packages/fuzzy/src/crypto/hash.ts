09a132ca0928a0518ed4c94039b3812d1bdf0ab2
murukal <tutu@fantufantu.com>
2024-10-23 23:03:55 +0800

    feat: hash sync

--- packages/fuzzy/src/crypto/hash.ts
+++ packages/fuzzy/src/crypto/hash.ts
@@ -10,3 +10,26 @@ export const hash = async (
   const _buffer = Array.from(new Uint8Array(buffer));
   return _buffer.map((_) => _.toString(16).padStart(2, "0")).join("");
 };
+
+/**
+ * @description
+ * sync function
+ */
+function* _hashSync(
+  input: string,
+  algorithm: AlgorithmIdentifier,
+): Generator<Promise<string>, string> {
+  const _hash = yield hash(input, algorithm);
+  return _hash;
+}
+
+const hashSync = (
+  input: string = crypto.randomUUID(),
+  algorithm: AlgorithmIdentifier = "SHA-1",
+) => {
+  const nextable = _hashSync(input, algorithm);
+
+  while (!nextable.next().done) {}
+
+  nextable.next().value;
+};
