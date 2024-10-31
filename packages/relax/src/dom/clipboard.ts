f2eb2d240c386900de9fc496c7c12054e4105591
murukal <tutu@fantufantu.com>
2024-10-31 22:20:33 +0800

    feat: add clipboard export

--- packages/relax/src/dom/clipboard.ts
+++ packages/relax/src/dom/clipboard.ts
@@ -1,5 +1,5 @@
 const clipboard = async (value: string) => {
-  await navigator.clipboard.writeText(value);
+  return await navigator.clipboard.writeText(value);
 };
 
 export { clipboard };
--- packages/relax/src/dom/index.ts
+++ packages/relax/src/dom/index.ts
@@ -1,5 +1,7 @@
 import { scrollTo, type Orientation } from "./scroll-to";
 import { contains, type Containable } from "./contains";
+import { clipboard } from "./clipboard";
 
+export { clipboard };
 export { scrollTo, contains };
 export type { Containable, Orientation };
