c01763a49dcf997df71d8ace13110b03e61b119b
murukal <tutu@fantufantu.com>
2025-02-04 22:46:35 +0800

    feat: update mountRef to accept voidable refable parameter

--- packages/relax/src/react/mount-ref.ts
+++ packages/relax/src/react/mount-ref.ts
@@ -1,9 +1,15 @@
 import type { MutableRefObject, RefCallback } from "react";
 import { isFunction } from "../../src/is/is-function";
+import { Voidable } from "../types";
+import { isVoid } from "../is/is-void";
 
 export type Refable<T> = RefCallback<T> | MutableRefObject<T> | string;
 
-export const mountRef = <T>(refable: Refable<T>, reference: T) => {
+export const mountRef = <T>(refable: Voidable<Refable<T>>, reference: T) => {
+  if (isVoid(refable)) {
+    return;
+  }
+
   if (isFunction(refable)) {
     refable(reference);
     return;
