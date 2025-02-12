f30af3b663a3fbdb0e576dbe738e9230d16a0c16
murukal <tutu@fantufantu.com>
2025-02-12 19:26:48 +0800

    feat: integrate Tailwind CSS with Vite configuration and update package dependencies

--- packages/wasp/src/configurations/vite.config.ts
+++ packages/wasp/src/configurations/vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
     port: 9527,
   },
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
+  plugins: [react(), tailwindcss()],
 });
