a812f366cc523e97980186cab9d092ab7127bfb5
murukal <tutu@fantufantu.com>
2025-04-20 11:45:53 +0800

    feat: implement isDeepKey function and enhance get utility for deep property access

---
+++ packages/relax/src/utils/to-key.ts
@@ -0,0 +1,18 @@
+/**
+ * Converts `value` to a string key if it's not a string or symbol.
+ *
+ * @private
+ * @param {unknown} value The value to inspect.
+ * @returns {string|symbol} Returns the key.
+ */
+export function toKey(value: unknown): string | symbol {
+  if (typeof value === "string" || typeof value === "symbol") {
+    return value;
+  }
+
+  if (Object.is(value?.valueOf?.(), -0)) {
+    return "-0";
+  }
+
+  return String(value);
+}
