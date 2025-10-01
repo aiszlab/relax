2d41106aa8932997209532fcea486be042929d15
murukal <tutu@fantufantu.com>
2025-10-01 22:51:46 +0800

    fix(relax): 更新版本至 2.0.8-beta.1，并优化 useEvent 钩子

--- packages/relax/src/hooks/use-event.ts
+++ packages/relax/src/hooks/use-event.ts
@@ -1,12 +1,10 @@
-import { useRef, useCallback, useEffect } from "react";
+import { useRef, useCallback } from "react";
 import type { AnyFunction } from "@aiszlab/relax/types";
 
 export const useEvent = <T extends AnyFunction>(callback: T) => {
   const ref = useRef<T>(callback);
 
-  useEffect(() => {
-    ref.current = callback;
-  });
+  ref.current = callback;
 
   return useCallback(
     ((...args: Parameters<AnyFunction>) => ref.current(...args)) as unknown as T,
